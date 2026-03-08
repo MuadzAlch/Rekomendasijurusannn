28dfb0a1c6b01114d17530ad69a3c6cadf2c93bd
brokenworld044 <muadzalchairi04@gmail.com>
2026-03-08 15:28:21 +0700

    Update app.py

--- app.py
+++ app.py
@@ -20,15 +20,13 @@ app.secret_key = 'supersecretkey'
 import os
 from urllib.parse import urlparse
 import mysql.connector
-
-def get_db_connection():
-    return mysql.connector.connect(
-        host="mysql.railway.internal",        # ganti sesuai host MySQL-mu
-        user="root",             # ganti sesuai username
-        password="KcUnfRnOLbppxwlzcqrRBvIzvOBhzmIS",   # ganti sesuai password
-        database="railway",       # ganti sesuai nama database
-        port=3306                # ganti sesuai port jika berbeda
-    )
+print("HOST:", os.environ.get("MYSQLHOST"))
+print("USER:", os.environ.get("MYSQLUSER"))
+print("PASS:", os.environ.get("MYSQLPASSWORD"))
+print("DB:", os.environ.get("MYSQLDATABASE"))
+print("PORT:", os.environ.get("MYSQLPORT"))
+def get_db_connection(): return mysql.connector.connect( host=os.environ.get("MYSQLHOST"), user=os.environ.get("MYSQLUSER"), password=os.environ.get("MYSQLPASSWORD"), database=os.environ.get("MYSQLDATABASE"), port=int(os.environ.get("MYSQLPORT", 3306)) )
+    
 # ================= LOAD MODEL =================
 model = joblib.load('model/model_rekomendasi_smk.pkl')
 
